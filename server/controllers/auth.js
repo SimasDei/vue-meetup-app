96187b49f78e208c858e57b782100eba95a9b62f
Simas <simonas@digitouch.lt>
2019-06-14 11:17:27 +0300

    add logout route and user authentication middleware

---
+++ server/controllers/auth.js
@@ -0,0 +1,4 @@
+exports.onlyAuthUser = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  return res.status(401).send({ errors: { auth: 'Not Authenticated' } });
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,7 +6,7 @@ const UsersCtrl = require('../controllers/users');
 router.get('', UsersCtrl.getUsers);
 
 router.post('/register', UsersCtrl.register);
-
 router.post('/login', UsersCtrl.login);
+router.post('/logout', UsersCtrl.logout);
 
 module.exports = router;
