ec089cf1b6a7fc293b1104c9f52cf81fd541cd4d
Simas <simonas@digitouch.lt>
2019-06-17 07:52:11 +0300

    implement JWT passport strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -38,7 +38,7 @@ passport.use(
 );
 
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.JWT_SECRET,
 };
 
