98b46ba3073f8f73d3492fdaabbc6ed7b349a07a
Simas <simonas@digitouch.lt>
2019-07-01 17:02:58 +0300

    add geoip-lite package and setup. Calling base api endpoint gives users current location data

--- server/controllers/meetups.js
+++ server/controllers/meetups.js
@@ -4,12 +4,27 @@ const User = require('../models/users');
 exports.getSecret = (req, res) => res.json({ secret: 'I am a secret message' });
 
 exports.getMeetups = function(req, res) {
-  Meetup.find({})
+  const { category } = req.query || {};
+  const { location } = req.query || {};
+
+  const findQuery = location
+    ? Meetup.find({ processedLocation: { $regex: '.*' + location + '.*' } })
+    : Meetup.find({});
+  findQuery
     .populate('category')
     .populate('joinedPeople')
+    .limit(5)
+    .sort({ createdAt: -1 })
     .exec((errors, meetups) => {
       if (errors) {
         return res.status(422).send({ errors });
+      }
+
+      // WARNING: requires improvement, can decrease performance
+      if (category) {
+        meetups = meetups.filter(meetup => {
+          return meetup.category.name === category;
+        });
       }
 
       return res.json(meetups);
--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ const meetupsRoutes = require('./routes/meetups'),
   usersRoutes = require('./routes/users'),
   threadsRoutes = require('./routes/threads'),
   postsRoutes = require('./routes/posts'),
-  categoriesRoutes = require('./routes/categories');
+  categoriesRoutes = require('./routes/categories'),
+  apiRoutes = require('./routes/api');
 
 mongoose
   .connect(config.DB_URI, { useNewUrlParser: true, useCreateIndex: true })
@@ -59,6 +60,7 @@ app.use(passport.initialize());
 app.use(passport.session());
  */
 
+app.use('/api/v1', apiRoutes);
 app.use('/api/v1/meetups', meetupsRoutes);
 app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/posts', postsRoutes);
