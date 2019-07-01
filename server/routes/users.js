bbc2378e79d51fd9fe90436169b5c9d3171f183c
Simas <simonas@digitouch.lt>
2019-07-01 14:40:29 +0300

    add Profile page markup and styling

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,6 +7,9 @@ const AuthCtrl = require('../controllers/auth');
 router.get('', UsersCtrl.getUsers);
 router.get('/me', AuthCtrl.onlyAuthUser, UsersCtrl.getCurrentUser);
 
+router.get('/me/activity', AuthCtrl.onlyAuthUser, UsersCtrl.getUserActivity);
+router.get('/:id', AuthCtrl.onlyAuthUser, UsersCtrl.updateUser);
+
 router.post('/register', UsersCtrl.register);
 router.post('/login', UsersCtrl.login);
 router.post('/logout', UsersCtrl.logout);
